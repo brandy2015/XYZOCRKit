634cb6565276aa8ba3c1cc33b301e6c28a19d02c
XYZTech <342994630@qq.com>
2025-07-18 09:27:03 +0800

    Initial Commit

---
+++ XYZOCRKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XYZOCRKit
+//
+//  Created by 张子豪 on 2025/7/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
